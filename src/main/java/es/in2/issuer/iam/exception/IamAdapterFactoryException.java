5a4ea9fe62559fa345d4ef8f9afb7ac83a44bc50
Dmitri Sidorov <dmitri.sidorov@in2.es>
2024-03-06 11:22:08 +0100

    ADD: custom exception for IAM adapter

---
+++ src/main/java/es/in2/issuer/iam/exception/IamAdapterFactoryException.java
@@ -0,0 +1,8 @@
+package es.in2.issuer.iam.exception;
+
+public class IamAdapterFactoryException extends RuntimeException {
+    private static final String MESSAGE = "Error creating IamAdapterFactory. There should be only one IamAdapter. Found: ";
+    public IamAdapterFactoryException(int size) {
+        super(MESSAGE + size);
+    }
+}
--- src/main/java/es/in2/issuer/iam/util/IamAdapterFactory.java
+++ src/main/java/es/in2/issuer/iam/util/IamAdapterFactory.java
@@ -1,5 +1,6 @@
 package es.in2.issuer.iam.util;
 
+import es.in2.issuer.iam.exception.IamAdapterFactoryException;
 import es.in2.issuer.iam.service.GenericIamAdapter;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +17,7 @@ public class IamAdapterFactory {
     public GenericIamAdapter getAdapter() {
         //check if list is empty or size is greater than 1
         if (iamAdapters.isEmpty() || iamAdapters.size() > 1) {
-            throw new RuntimeException("Invalid number of config services found");
+            throw new IamAdapterFactoryException(iamAdapters.size());
         }
 
         return iamAdapters.get(0);
