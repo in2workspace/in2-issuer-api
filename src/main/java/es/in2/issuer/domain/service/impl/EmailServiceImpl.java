8ccf97e592b4020d2ad08cc18825586e315f2c6d
Dmitri Sidorov <dmitri.sidorov@in2.es>
2024-05-22 13:35:32 +0200

    implement template for email

--- src/main/java/es/in2/issuer/domain/service/impl/EmailServiceImpl.java
+++ src/main/java/es/in2/issuer/domain/service/impl/EmailServiceImpl.java
@@ -1,11 +1,15 @@
 package es.in2.issuer.domain.service.impl;
 
 import es.in2.issuer.domain.service.EmailService;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -16,15 +20,23 @@ import static es.in2.issuer.domain.util.Constants.NO_REPLAY_EMAIL;
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
     private final JavaMailSender javaMailSender;
+    private final TemplateEngine templateEngine;
+
     @Override
     public Mono<Void> sendPin(String to, String subject, String pin) {
         return Mono.fromCallable(() -> {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom(NO_REPLAY_EMAIL);
-            message.setTo(to);
-            message.setSubject(subject);
-            message.setText(pin);
-            javaMailSender.send(message);
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            helper.setFrom(NO_REPLAY_EMAIL);
+            helper.setTo(to);
+            helper.setSubject(subject);
+
+            Context context = new Context();
+            context.setVariable("pin", pin);
+            String htmlContent = templateEngine.process("pin-email", context);
+            helper.setText(htmlContent, true);
+
+            javaMailSender.send(mimeMessage);
             return null;
         }).subscribeOn(Schedulers.boundedElastic()).then();
     }
