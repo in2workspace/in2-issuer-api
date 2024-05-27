bddc1263e169ccd727e668198c510b70ad869218
Dmitri Sidorov <dmitri.sidorov@in2.es>
2024-05-27 08:48:49 +0200

    update endpoints

--- src/main/java/es/in2/issuer/domain/util/EndpointsConstants.java
+++ src/main/java/es/in2/issuer/domain/util/EndpointsConstants.java
@@ -7,9 +7,9 @@ public class EndpointsConstants {
     public static final String CREDENTIAL_OFFER = "/api/v1/credential-offer/";
     public static final String LEAR_CREDENTIAL_URI = "/api/v1/credential-offer?credential_offer_uri=";
     public static final String OPENID_CREDENTIAL_OFFER = "openid-credential-offer://?credential_offer_uri=";
-    public static final String CREDENTIAL = "/api/v1/vc/credential";
-    public static final String CREDENTIAL_BATCH = "/api/v1/vc/batch_credential";
-    public static final String CREDENTIAL_DEFERRED = "/api/v1/vc/deferred_credential";
+    public static final String CREDENTIAL = "/api/v1/credentials/request-credential";
+    public static final String CREDENTIAL_BATCH = "/api/v1/credentials/request-batch-credential";
+    public static final String CREDENTIAL_DEFERRED = "/api/v1/credentials/request-deferred-credential";
     public static final String PUBLIC_HEALTH = "/health";
     public static final String PUBLIC_CREDENTIAL_OFFER = "/api/v1/credential-offer/**";
     public static final String PUBLIC_DISCOVERY_ISSUER = "/.well-known/openid-credential-issuer";
--- src/main/java/es/in2/issuer/infrastructure/controller/CredentialController.java
+++ src/main/java/es/in2/issuer/infrastructure/controller/CredentialController.java
@@ -48,32 +48,32 @@ public class CredentialController {
         return verifiableCredentialIssuanceWorkflow.completeWithdrawLearCredentialProcess(processId, type, learCredentialRequest);
     }
 
-//    @Operation(
-//            summary = "Generate a new Verifiable Credential",
-//            description = "Generate a new Verifiable Credential and returns it with its id (nonce) assigned, lifetime in seconds and format",
-//            tags = {SwaggerConfig.TAG_PRIVATE}
-//    )
-//    @ApiResponses(
-//            value = {
-//                    @ApiResponse(responseCode = "201", description = "Returns an URL with a linked ID parameter to retrieve the created Verifiable Credential."),
-//                    @ApiResponse(responseCode = "404", description = "Credential Request did not contain a proof, proof was invalid or no grants retrieved for the given user", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CredentialErrorResponse.class),
-//                            examples = @ExampleObject(name = "invalidOrMissingProof", value = "{\"error\": \"invalid_or_missing_proof\", \"description\": \"Credential Request did not contain a proof, or proof was invalid\"}"))),
-//                    @ApiResponse(responseCode = "500", description = "This response is returned when an unexpected server error occurs. It includes an error message if one is available.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = GlobalErrorMessage.class)))
-//            }
-//    )
-//    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-//    @ResponseStatus(HttpStatus.CREATED)
-//    // fixme: repensar esta API
-//    public Mono<VerifiableCredentialResponse> createVerifiableCredential(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody CredentialRequest credentialRequest) {
-//        String processId = UUID.randomUUID().toString();
-//        return accessTokenService.getCleanBearerToken(authorizationHeader)
-//                .flatMap(token -> accessTokenService.getUserIdFromHeader(authorizationHeader)
-//                        .flatMap(userId -> verifiableCredentialIssuanceWorkflow.generateVerifiableCredentialResponse(userId, credentialRequest, token)))
-//                .doOnNext(result -> log.info("VerifiableCredentialController - createVerifiableCredential()"));
-//    }
+    @Operation(
+            summary = "Generate a new Verifiable Credential",
+            description = "Generate a new Verifiable Credential and returns it with its id (nonce) assigned, lifetime in seconds and format",
+            tags = {SwaggerConfig.TAG_PRIVATE}
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "201", description = "Returns an URL with a linked ID parameter to retrieve the created Verifiable Credential."),
+                    @ApiResponse(responseCode = "404", description = "Credential Request did not contain a proof, proof was invalid or no grants retrieved for the given user", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = CredentialErrorResponse.class),
+                            examples = @ExampleObject(name = "invalidOrMissingProof", value = "{\"error\": \"invalid_or_missing_proof\", \"description\": \"Credential Request did not contain a proof, or proof was invalid\"}"))),
+                    @ApiResponse(responseCode = "500", description = "This response is returned when an unexpected server error occurs. It includes an error message if one is available.", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = GlobalErrorMessage.class)))
+            }
+    )
+    @PostMapping(value = "/request-credential", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    // fixme: repensar esta API
+    public Mono<VerifiableCredentialResponse> createVerifiableCredential(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody CredentialRequest credentialRequest) {
+        String processId = UUID.randomUUID().toString();
+        return accessTokenService.getCleanBearerToken(authorizationHeader)
+                .flatMap(token -> accessTokenService.getUserIdFromHeader(authorizationHeader)
+                        .flatMap(userId -> verifiableCredentialIssuanceWorkflow.generateVerifiableCredentialResponse(userId, credentialRequest, token)))
+                .doOnNext(result -> log.info("VerifiableCredentialController - createVerifiableCredential()"));
+    }
 
 
-    @PostMapping(value = "/deferred_credential", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/request-deferred-credential", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<VerifiableCredentialResponse> getCredential(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody DeferredCredentialRequest deferredCredentialRequest) {
         String processId = UUID.randomUUID().toString();
@@ -81,7 +81,7 @@ public class CredentialController {
                 .doOnNext(result -> log.info("VerifiableCredentialController - getDeferredCredential()"));
     }
 
-    @PostMapping(value = "/batch_credential", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/request-batch-credential", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<BatchCredentialResponse> createVerifiableCredentials(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody BatchCredentialRequest batchCredentialRequest) {
         return accessTokenService.getCleanBearerToken(authorizationHeader)
