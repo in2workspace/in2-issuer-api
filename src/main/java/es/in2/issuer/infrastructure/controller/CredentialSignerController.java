7806c8a2990547de90bcd57d9965655cf3f3a103
Albert Rodríguez <albert.rodriguez@in2.es>
2024-07-04 11:05:28 +0200

    fix(signer):  add logs

--- src/main/java/es/in2/issuer/infrastructure/controller/CredentialSignerController.java
+++ src/main/java/es/in2/issuer/infrastructure/controller/CredentialSignerController.java
@@ -17,11 +17,11 @@ public class CredentialSignerController {
 
     private final CredentialSignerWorkflow credentialSignerWorkflow;
 
-    @PostMapping(value = "/{procedureId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/{procedure_id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Void> createVerifiableCredential(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader,
-            @PathVariable String procedureId) {
+            @PathVariable("procedure_id") String procedureId) {
         return credentialSignerWorkflow.signCredential(authorizationHeader, procedureId);
     }
 }
