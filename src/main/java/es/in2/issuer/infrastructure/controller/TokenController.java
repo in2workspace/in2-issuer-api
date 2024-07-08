bf118ef230d2a3bd9524dd1a3b1c370b37dfb455
Albert Rodríguez <albert.rodriguez@in2.es>
2024-07-08 19:00:32 +0200

    feat(token): add send issuer keycloak

--- src/main/java/es/in2/issuer/infrastructure/controller/TokenController.java
+++ src/main/java/es/in2/issuer/infrastructure/controller/TokenController.java
@@ -17,51 +17,36 @@ import reactor.netty.resources.ConnectionProvider;
 import java.net.URI;
 import java.time.Duration;
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @RestController
 @RequestMapping("/token")
 public class TokenController {
 
-   /* private static final ConnectionProvider connectionProvider = ConnectionProvider.builder("custom")
-            .maxConnections(500)
-            .maxIdleTime(Duration.ofSeconds(50))
-            .maxLifeTime(Duration.ofSeconds(300))
-            .evictInBackground(Duration.ofSeconds(80))
-            .build();
-
-    @PostMapping(value = "/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public Mono<Void> tokenBypass(@RequestBody Map<String, String> formData,
-                                    @RequestHeader("Content-Type") String contentType) {
-
-        log.info("Received token request with form data: {}", formData);
-        log.info("Received token request with content type: {}", contentType);
-*/
-//        WebClient webClient = WebClient.builder()
-//                .clientConnector(new ReactorClientHttpConnector(
-//                        HttpClient.create(connectionProvider).followRedirect(false))
-//                )
-//                .build();
-
-//        return webClient.post()
-//                .uri(URI.create("https://in2-dome-marketplace-test.org/issuer-keycloak/realms/CredentialIssuer/verifiable-credential/did:key:z6MkqmaCT2JqdUtLeKah7tEVfNXtDXtQyj4yxEgV11Y5CqUa/token"))
-//                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-//                .body(BodyInserters.fromFormData(new LinkedMultiValueMap<>()))
-//                .retrieve()
-//                .bodyToMono(Object.class);
-
-        /*return Mono.empty();
-    }*/
-
     @PostMapping
     public Mono<String> handleData(ServerWebExchange exchange) {
         Mono<MultiValueMap<String, String>> formDataMono = exchange.getFormData();
 
-        return formDataMono.map(formData -> {
-            log.debug("\n==================================\n");
-            log.debug("TokenFormData:[{}]", formData);
-            log.debug("\n==================================\n");
-            return formData.toString();
+        log.info("Get formDataMono. [Exchange:{}]", exchange);
+
+        return formDataMono.flatMap(formData -> {
+            log.info("\n==================================\n");
+            log.info("[TokenFormData:{}]", formData);
+            log.info("\n==================================\n");
+
+            var client = WebClient.builder()
+                    .baseUrl("http://dome-issuer-keycloak:8080")
+                    .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+                    .build();
+
+            return client.post()
+                    .uri("issuer-keycloak/realms/CredentialIssuer/verifiable-credential/did:key:z6MkqmaCT2JqdUtLeKah7tEVfNXtDXtQyj4yxEgV11Y5CqUa/token")
+                    .body(BodyInserters.fromFormData("grant_type", Objects.requireNonNull(formData.getFirst("grant_type")))
+                            .with("pre-authorized_code", Objects.requireNonNull(formData.getFirst("pre-authorized_code")))
+                            .with("tx_code", Objects.requireNonNull(formData.getFirst("tx_code"))))
+                    .retrieve()
+                    .bodyToMono(String.class);
         });
     }
 }
