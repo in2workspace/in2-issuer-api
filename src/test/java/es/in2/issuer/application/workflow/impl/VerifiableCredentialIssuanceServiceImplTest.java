807dc00072fbfe281e58f094a30e26c0c6a95957
Dmitri Sidorov Manko <159002112+dmitriin2@users.noreply.github.com>
2024-09-18 20:20:38 +0200

    add tests

--- src/test/java/es/in2/issuer/application/workflow/impl/VerifiableCredentialIssuanceServiceImplTest.java
+++ src/test/java/es/in2/issuer/application/workflow/impl/VerifiableCredentialIssuanceServiceImplTest.java
@@ -123,6 +123,62 @@ class VerifiableCredentialIssuanceServiceImplTest {
     }
 
     @Test
+    void completeWithdrawVerifiableCertificationProcessSuccess() throws JsonProcessingException {
+        String processId = "1234";
+        String type = "VerifiableCertification";
+        String procedureId = "procedureId";
+        String token = "token";
+        String json = """
+                {
+                    "type": [
+                        "ProductOfferingCredential"
+                    ],
+                    "issuer": {
+                        "commonName": "IssuerCommonName",
+                        "country": "ES",
+                        "id": "did:web:issuer-test.com",
+                        "organization": "Issuer Test"
+                    },
+                    "credentialSubject": {
+                        "company": {
+                            "address": "address",
+                            "commonName": "commonName",
+                            "country": "ES",
+                            "email": "email@email.com",
+                            "id": "did:web:commonname.com",
+                            "organization": "Organization Name"
+                        },
+                        "compliance": [
+                            {
+                                "scope": "Scope Name",
+                                "standard": "standard"
+                            }
+                        ],
+                        "product": {
+                            "productId": "productId",
+                            "productName": "Product Name",
+                            "productVersion": "0.1"
+                        }
+                    },
+                    "issuanceDate": "2024-08-22T00:00:00Z",
+                    "validFrom": "2024-08-22T00:00:00Z",
+                    "expirationDate": "2025-08-22T00:00:00Z"
+                }
+                """;
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+        IssuanceRequest issuanceRequest = IssuanceRequest.builder().payload(jsonNode).schema("VerifiableCertification").operationMode("S").build();
+        String transactionCode = "4321";
+
+        when(verifiableCredentialService.generateVc(processId,type, issuanceRequest)).thenReturn(Mono.just(transactionCode));
+        when(deferredCredentialMetadataService.getProcedureIdByTransactionCode(transactionCode)).thenReturn(Mono.just(procedureId));
+        when(credentialSignerWorkflow.signAndUpdateCredential(token, procedureId)).thenReturn(Mono.just("signedCredential"));
+
+        StepVerifier.create(verifiableCredentialIssuanceWorkflow.completeIssuanceCredentialProcess(processId,type, issuanceRequest, token))
+                .verifyComplete();
+    }
+
+    @Test
     void completeWithdrawSyncProcessSuccess() throws JsonProcessingException {
         String processId = "1234";
         String type = "LEARCredentialEmployee";
@@ -186,8 +242,6 @@ class VerifiableCredentialIssuanceServiceImplTest {
         String walletUrl = "https://wallet.com";
 
         when(verifiableCredentialService.generateVc(processId,type, issuanceRequest)).thenReturn(Mono.just(transactionCode));
-//        when(deferredCredentialMetadataService.getProcedureIdByTransactionCode(transactionCode)).thenReturn(Mono.just("procedureId"));
-//        when(credentialSignerWorkflow.signAndUpdateCredential("token", "procedureId")).thenReturn(Mono.just("encodedCredential"));
         when(appConfig.getIssuerUiExternalDomain()).thenReturn(issuerUIExternalDomain);
         when(appConfig.getWalletUrl()).thenReturn(walletUrl);
         when(emailService.sendTransactionCodeForCredentialOffer("example@in2.es","Credential Offer",issuerUIExternalDomain + "/credential-offer?transaction_code=" + transactionCode, "Jhon",walletUrl)).thenReturn(Mono.empty());
@@ -223,6 +277,40 @@ class VerifiableCredentialIssuanceServiceImplTest {
         when(credentialProcedureService.getSignerEmailFromDecodedCredentialByProcedureId(procedureId)).thenReturn(Mono.just(mandatorEmail));
         when(emailService.sendPendingCredentialNotification(mandatorEmail,"Pending Credential")).thenReturn(Mono.empty());
         when(deferredCredentialMetadataService.getOperationModeByAuthServerNonce(jti)).thenReturn(Mono.just("A"));
+
+        StepVerifier.create(verifiableCredentialIssuanceWorkflow.generateVerifiableCredentialResponse(processId,credentialRequest, token))
+                .expectNext(verifiableCredentialResponse)
+                .verifyComplete();
+    }
+
+    @Test
+    void generateVerifiableCredentialResponseSyncSuccess(){
+        String processId = "1234";
+        CredentialRequest credentialRequest = CredentialRequest.builder()
+                .format(JWT_VC)
+                .credentialIdentifier("LEARCredentialEmployee")
+                .proof(Proof.builder()
+                        .proofType("jwt")
+                        .jwt("eyJraWQiOiJkaWQ6a2V5OnpEbmFlbjIzd003NmdwaVNMSGt1NGJGRGJzc1ZTOXN0eTl4M0s3eVZxamJTZFRQV0MjekRuYWVuMjN3TTc2Z3BpU0xIa3U0YkZEYnNzVlM5c3R5OXgzSzd5VnFqYlNkVFBXQyIsInR5cCI6Im9wZW5pZDR2Y2ktcHJvb2Yrand0IiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJkaWQ6a2V5OnpEbmFlbjIzd003NmdwaVNMSGt1NGJGRGJzc1ZTOXN0eTl4M0s3eVZxamJTZFRQV0MiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwNzEiLCJleHAiOjE3MTI5MTcwNDAsImlhdCI6MTcxMjA1MzA0MCwibm9uY2UiOiI4OVh4bXdMMlJtR2wyUlp1LU1UU3lRPT0ifQ.DdaaNm4vTn60njLtAQ7Q5oGsQILfA-5h9-sv4MBcVyNBAfSrUUajZqlUukT-5Bx8EqocSvf0RIFRHLcvO9_LMg")
+                        .build())
+                .build();
+        String token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICIyQ1ltNzdGdGdRNS1uU2stU3p4T2VYYUVOUTRoSGRkNkR5U2NYZzJFaXJjIn0.eyJleHAiOjE3MTAyNDM2MzIsImlhdCI6MTcxMDI0MzMzMiwiYXV0aF90aW1lIjoxNzEwMjQwMTczLCJqdGkiOiJmY2NhNzU5MS02NzQyLTRjMzAtOTQ5Yy1lZTk3MDcxOTY3NTYiLCJpc3MiOiJodHRwczovL2tleWNsb2FrLXByb3ZpZGVyLmRvbWUuZml3YXJlLmRldi9yZWFsbXMvZG9tZSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJlMmEwNjZmNS00YzAwLTQ5NTYtYjQ0NC03ZWE1ZTE1NmUwNWQiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJhY2NvdW50LWNvbnNvbGUiLCJzZXNzaW9uX3N0YXRlIjoiYzFhMTUyYjYtNWJhNy00Y2M4LWFjOTktN2Q2ZTllODIyMjk2IiwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyJdfX0sInNjb3BlIjoib3BlbmlkIGVtYWlsIHByb2ZpbGUiLCJzaWQiOiJjMWExNTJiNi01YmE3LTRjYzgtYWM5OS03ZDZlOWU4MjIyOTYiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsIm5hbWUiOiJQcm92aWRlciBMZWFyIiwicHJlZmVycmVkX3VzZXJuYW1lIjoicHJvdmlkZXItbGVhciIsImdpdmVuX25hbWUiOiJQcm92aWRlciIsImZhbWlseV9uYW1lIjoiTGVhciJ9.F8vTSNAMc5Fmi-KO0POuaMIxcjdpWxNqfXH3NVdQP18RPKGI5eJr5AGN-yKYncEEzkM5_H28abJc1k_lx7RjnERemqesY5RwoBpTl9_CzdSFnIFbroNOAY4BGgiU-9Md9JsLrENk5Na_uNV_Q85_72tmRpfESqy5dMVoFzWZHj2LwV5dji2n17yf0BjtaWailHdwbnDoSqQab4IgYsExhUkCLCtZ3O418BG9nrSvP-BLQh_EvU3ry4NtnnWxwi5rNk4wzT4j8rxLEAJpMMv-5Ew0z7rbFX3X3UW9WV9YN9eV79-YrmxOksPYahFQwNUXPckCXnM48ZHZ42B0H4iOiA";
+        String jti = "fcca7591-6742-4c30-949c-ee9707196756";
+        String did = "did:key:zDnaen23wM76gpiSLHku4bFDbssVS9sty9x3K7yVqjbSdTPWC";
+        VerifiableCredentialResponse verifiableCredentialResponse = VerifiableCredentialResponse.builder()
+                .credential("credential")
+                .transactionId("4321")
+                .build();
+        String procedureId = "123456";
+
+        when(proofValidationService.isProofValid(credentialRequest.proof().jwt(), token)).thenReturn(Mono.just(true));
+        when(verifiableCredentialService.buildCredentialResponse(processId,did,jti,credentialRequest.format(), token, "S")).thenReturn(Mono.just(verifiableCredentialResponse));
+        when(deferredCredentialMetadataService.getProcedureIdByAuthServerNonce(jti)).thenReturn(Mono.just(procedureId));
+        when(deferredCredentialMetadataService.getOperationModeByAuthServerNonce(jti)).thenReturn(Mono.just("S"));
+
+        when(deferredCredentialMetadataService.getProcedureIdByAuthServerNonce(jti)).thenReturn(Mono.just("procedureId"));
+        when(credentialProcedureService.updateCredentialProcedureCredentialStatusToValidByProcedureId("procedureId")).thenReturn(Mono.empty());
+        when(deferredCredentialMetadataService.deleteDeferredCredentialMetadataByAuthServerNonce(jti)).thenReturn(Mono.empty());
 
         StepVerifier.create(verifiableCredentialIssuanceWorkflow.generateVerifiableCredentialResponse(processId,credentialRequest, token))
                 .expectNext(verifiableCredentialResponse)
