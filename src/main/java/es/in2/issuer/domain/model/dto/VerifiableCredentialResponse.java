d9ca8ce66c66a2c974708515c548ccb3371783e0
Dmitri Sidorov Manko <159002112+dmitriin2@users.noreply.github.com>
2024-09-22 17:34:37 +0200

    add include non null to VerifiableCredentialResponse

--- src/main/java/es/in2/issuer/domain/model/dto/VerifiableCredentialResponse.java
+++ src/main/java/es/in2/issuer/domain/model/dto/VerifiableCredentialResponse.java
@@ -1,4 +1,5 @@
 package es.in2.issuer.domain.model.dto;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -8,6 +9,7 @@ import lombok.Builder;
     Implements the credential response according to 
     https://github.com/hesusruiz/EUDIMVP/blob/main/issuance.md#credential-response
     """)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record VerifiableCredentialResponse(
         //@Schema(example = "jwt_vc_json", description = "Format of the issued Credential.") @JsonProperty("format") String format,
         @Schema(example = "LUpixVCWJk0eOt4CXQe1NXK....WZwmhmn9OQp6YxX0a2L", description = "Contains issued Credential") @JsonProperty("credential") String credential,
