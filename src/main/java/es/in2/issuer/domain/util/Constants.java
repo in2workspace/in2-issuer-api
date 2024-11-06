8060d032a41e42975ec88e2620e4bff4d0e270fa
javieralvarezin2 <javier.alvarez@in2.es>
2024-11-06 10:58:35 +0100

    change @context value

--- src/main/java/es/in2/issuer/domain/util/Constants.java
+++ src/main/java/es/in2/issuer/domain/util/Constants.java
@@ -39,7 +39,7 @@ public class Constants {
     public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
     public static final String CONTENT_TYPE_URL_ENCODED_FORM = "application/x-www-form-urlencoded";
     public static final String DID_ELSI = "did:elsi:";
-    public static final List<String> CREDENTIAL_CONTEXT = List.of("https://www.w3.org/ns/credentials/v2","https://dome-marketplace.eu/2022/credentials/learcredential/v1");
+    public static final List<String> CREDENTIAL_CONTEXT = List.of("https://www.w3.org/ns/credentials/v2","https://trust-framework.dome-marketplace.eu/credentials/learcredentialemployee/v1");
     public static final String ASYNC = "A";
     public static final String SYNC = "S";
 }
