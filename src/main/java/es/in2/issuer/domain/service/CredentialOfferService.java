9c35f45899ca0f0dfb97bd59bfb7e8d4d829c76b
RubenModamioGarcia <ruben.modamio@in2.es>
2024-12-23 16:45:10 +0100

    remove unused imports

--- src/main/java/es/in2/issuer/domain/service/CredentialOfferService.java
+++ src/main/java/es/in2/issuer/domain/service/CredentialOfferService.java
@@ -1,7 +1,6 @@
 package es.in2.issuer.domain.service;
 
 import es.in2.issuer.domain.model.dto.CredentialOfferData;
-import es.in2.issuer.domain.model.dto.CustomCredentialOffer;
 import es.in2.issuer.domain.model.dto.Grant;
 import reactor.core.publisher.Mono;
 
