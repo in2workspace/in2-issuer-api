71ca3f3588153c173af438e6f99cd960a9199e65
javieralvarezin2 <javier.alvarez@in2.es>
2024-12-23 13:06:28 +0100

    wip: add Draft credential status

--- src/main/java/es/in2/issuer/domain/model/enums/CredentialStatus.java
+++ src/main/java/es/in2/issuer/domain/model/enums/CredentialStatus.java
@@ -2,6 +2,7 @@ package es.in2.issuer.domain.model.enums;
 
 public enum CredentialStatus {
     WITHDRAWN,
+    DRAFT, // Old status. Necessary to maintain retro compatibility
     ISSUED,
     PEND_DOWNLOAD,
     VALID,
