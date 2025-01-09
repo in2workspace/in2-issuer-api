95ff1a2cb78499cac23a1e7c0dfd4b0cac51102b
RubenModamioGarcia <ruben.modamio@in2.es>
2025-01-09 08:44:05 +0100

    update sql migration file

--- src/main/resources/db/migration/V2__Add_columns_to_deferred_credential_metadata.sql
+++ src/main/resources/db/migration/V2__Add_columns_to_deferred_credential_metadata.sql
@@ -1,7 +1,3 @@
 ALTER TABLE credentials.deferred_credential_metadata
 ADD COLUMN operation_mode VARCHAR(20),
 ADD COLUMN response_uri VARCHAR(255);
-ALTER TABLE credentials.credential_procedure
-ADD COLUMN subject VARCHAR(255),
-ADD COLUMN valid_until TIMESTAMP,
-ADD COLUMN credential_type VARCHAR(50);
