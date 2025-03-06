94fbd39de92a70f5c08e9ca0b02fca1b86cea103
RubenModamioGarcia <ruben.modamio@in2.es>
2025-03-06 15:53:29 +0100

    add jsonValue

--- src/main/java/es/in2/issuer/domain/model/dto/SimpleIssuer.java
+++ src/main/java/es/in2/issuer/domain/model/dto/SimpleIssuer.java
@@ -1,10 +1,17 @@
 package es.in2.issuer.domain.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Builder;
 
 @Builder
 public record SimpleIssuer(@JsonProperty("id") String id) implements Issuer {
+
+    @JsonValue
+    public String toJson() {
+        return id;
+    }
+
     @Override
     public String getId() {
         return id;
