54d178619f48928b0005431e51ed633bf3973575
RubenModamioGarcia <ruben.modamio@in2.es>
2025-03-07 12:46:51 +0100

    add support for lear credential machine

--- src/main/java/es/in2/issuer/domain/model/dto/credential/lear/LifeSpan.java
+++ src/main/java/es/in2/issuer/domain/model/dto/credential/lear/LifeSpan.java
@@ -1,7 +1,6 @@
 package es.in2.issuer.domain.model.dto.credential.lear;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 
