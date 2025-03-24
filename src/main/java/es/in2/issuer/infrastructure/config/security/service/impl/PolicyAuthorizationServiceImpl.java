630baa5999d209181e09ab3a27812dc95a51360a
Victor Poquet <victor.poquet@in2.es>
2025-03-24 16:53:14 +0100

    External Request Error

--- src/main/java/es/in2/issuer/infrastructure/config/security/service/impl/PolicyAuthorizationServiceImpl.java
+++ src/main/java/es/in2/issuer/infrastructure/config/security/service/impl/PolicyAuthorizationServiceImpl.java
@@ -38,18 +38,12 @@ public class PolicyAuthorizationServiceImpl implements PolicyAuthorizationServic
     public Mono<Void> authorize(String token, String schema, JsonNode payload) {
         return Mono.fromCallable(() -> jwtService.parseJWT(token))
                 .flatMap(signedJWT -> {
-                    try {
-                        String payloadStr = signedJWT.getPayload().toString();
-                        ObjectMapper mapper = new ObjectMapper();
-                        JsonNode jsonPayload = mapper.readTree(payloadStr);
-                        if (jsonPayload.has(ROLE)) {
-                            String roleClaim = jwtService.getClaimFromPayload(signedJWT.getPayload(), ROLE);
-                            return authorizeByRole(roleClaim, token, schema, payload);
-                        }else{
-                            return checkPoliciesForLearOrExternalAccess(token, schema, payload);
-                        }
-                    } catch (JsonProcessingException e) {
-                        return Mono.error(new RuntimeException("Error processing JSON payload: " + e.getMessage(), e));
+                    String payloadStr = signedJWT.getPayload().toString();
+                    if (!payloadStr.contains(ROLE)) {
+                        return checkPoliciesForLearOrExternalAccess(token, schema, payload);
+                    }else{
+                        String roleClaim = jwtService.getClaimFromPayload(signedJWT.getPayload(), ROLE);
+                        return authorizeByRole(roleClaim, token, schema, payload);
                     }
                 });
     }
