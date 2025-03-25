ed2c3db24df01600b27a9e0914a10367554c58ef
RubenModamioGarcia <ruben.modamio@in2.es>
2025-03-25 16:16:21 +0100

    fix invalid flyway schema

--- src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
@@ -1,8 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE SCHEMA IF NOT EXISTS credentials;
+CREATE SCHEMA IF NOT EXISTS issuer;
 
 -- Create credential_management table if it doesn't exist
-CREATE TABLE IF NOT EXISTS credentials.credential_procedure (
+CREATE TABLE IF NOT EXISTS issuer.credential_procedure (
     procedure_id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
     credential_id uuid,
     credential_format VARCHAR(20),
@@ -10,20 +10,25 @@ CREATE TABLE IF NOT EXISTS credentials.credential_procedure (
     credential_encoded TEXT,
     credential_status VARCHAR(20),
     organization_identifier VARCHAR(255),
+    subject VARCHAR(255),
+    valid_until TIMESTAMP,
+    credential_type VARCHAR(50),
     updated_at TIMESTAMP
 );
 
 -- Create credential_deferred table if it doesn't exist
-CREATE TABLE IF NOT EXISTS credentials.deferred_credential_metadata (
+CREATE TABLE IF NOT EXISTS issuer.deferred_credential_metadata (
     id uuid PRIMARY KEY UNIQUE DEFAULT uuid_generate_v4(),
     transaction_code VARCHAR(255),
     transaction_id VARCHAR(255),
     auth_server_nonce VARCHAR(255),
+    operation_mode VARCHAR(20),
+    response_uri VARCHAR(255),
     procedure_id uuid,
     vc TEXT,
     vc_format VARCHAR(20),
     CONSTRAINT fk_credential_procedure
         FOREIGN KEY (procedure_id)
-        REFERENCES credentials.credential_procedure (procedure_id)
+        REFERENCES issuer.credential_procedure (procedure_id)
         ON DELETE CASCADE
 );
