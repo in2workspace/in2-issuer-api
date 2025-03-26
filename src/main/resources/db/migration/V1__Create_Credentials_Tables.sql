43c764a50e81e30db9e7ad86ce34b2dc7c2c9e4f
RubenModamioGarcia <ruben.modamio@in2.es>
2025-03-26 15:04:44 +0100

    fix sql file

--- src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
@@ -10,10 +10,10 @@ CREATE TABLE IF NOT EXISTS issuer.credential_procedure (
     credential_encoded TEXT,
     credential_status VARCHAR(20),
     organization_identifier VARCHAR(255),
+    updated_at TIMESTAMP,
     subject VARCHAR(255),
     valid_until TIMESTAMP,
     credential_type VARCHAR(50),
-    updated_at TIMESTAMP
 );
 
 -- Create credential_deferred table if it doesn't exist
@@ -22,11 +22,11 @@ CREATE TABLE IF NOT EXISTS issuer.deferred_credential_metadata (
     transaction_code VARCHAR(255),
     transaction_id VARCHAR(255),
     auth_server_nonce VARCHAR(255),
-    operation_mode VARCHAR(20),
-    response_uri VARCHAR(255),
     procedure_id uuid,
     vc TEXT,
     vc_format VARCHAR(20),
+    operation_mode VARCHAR(20),
+    response_uri VARCHAR(255),
     CONSTRAINT fk_credential_procedure
         FOREIGN KEY (procedure_id)
         REFERENCES issuer.credential_procedure (procedure_id)
