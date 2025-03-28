6d0c22aab89f13ea0fd0c3776d9a689e264f554d
RubenModamioGarcia <ruben.modamio@in2.es>
2025-03-28 15:01:20 +0100

    add serial to exceptions

--- src/main/java/es/in2/issuer/domain/exception/CredentialOfferEmailException.java
+++ src/main/java/es/in2/issuer/domain/exception/CredentialOfferEmailException.java
@@ -1,6 +1,10 @@
 package es.in2.issuer.domain.exception;
 
+import java.io.Serial;
+
 public class CredentialOfferEmailException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
     public CredentialOfferEmailException(String message) {
         super(message);
     }
