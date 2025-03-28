8b279bdc6bf64244a1ec02b1eba528a5cc5543d3
Albert Rodríguez <albert.rodriguez@in2.es>
2025-03-28 12:19:11 +0100

    test(domain): move test utils

--- src/test/java/es/in2/issuer/backend/domain/util/UtilsTest.java
+++ src/test/java/es/in2/issuer/backend/domain/util/UtilsTest.java
@@ -1,11 +1,9 @@
 package es.in2.issuer.backend.domain.util;
 
 import org.junit.jupiter.api.Test;
-import reactor.test.StepVerifier;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,16 +15,5 @@ class UtilsTest {
         InvocationTargetException invocationTargetException = assertThrows(InvocationTargetException.class, constructor::newInstance);
         assertInstanceOf(IllegalStateException.class, invocationTargetException.getCause());
         assertEquals("Utility class", invocationTargetException.getCause().getMessage());
-    }
-
-    @Test
-    void testGenerateCustomNonce() {
-        StepVerifier.create(es.in2.issuer.shared.domain.util.Utils.generateCustomNonce())
-                .assertNext(nonce -> {
-                    assertNotNull(nonce);
-                    assertFalse(nonce.isEmpty());
-                    assertDoesNotThrow(() -> Base64.getUrlDecoder().decode(nonce));
-                })
-                .verifyComplete();
     }
 }
