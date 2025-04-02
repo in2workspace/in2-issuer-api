ff7c34c982ac8b63c348be135e542236e01a732a
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-02 12:11:53 +0200

    add change

--- src/main/java/es/in2/issuer/domain/model/dto/GlobalErrorMessage.java
+++ src/main/java/es/in2/issuer/domain/model/dto/GlobalErrorMessage.java
@@ -1,5 +1,6 @@
 package es.in2.issuer.domain.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
@@ -8,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Builder
 public record GlobalErrorMessage(
-        @Schema(description = "Generic response when an unexpected error occurs") @JsonProperty("timestamp") LocalDateTime timestamp,
+        @Schema(description = "Generic response when an unexpected error occurs") @JsonProperty("timestamp") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime timestamp,
         @JsonProperty("status") int status, @JsonProperty("error") String error,
         @JsonProperty("message") String message, @JsonProperty("path") String path) {
 }
