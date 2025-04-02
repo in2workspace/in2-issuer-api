d199f87e017f47eef0def150a3fc4667e827ebbe
RubenModamioGarcia <ruben.modamio@in2.es>
2025-04-02 12:25:21 +0200

    add dependencies

--- src/main/java/es/in2/issuer/domain/model/dto/GlobalErrorMessage.java
+++ src/main/java/es/in2/issuer/domain/model/dto/GlobalErrorMessage.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @Builder
 public record GlobalErrorMessage(
-        @Schema(description = "Generic response when an unexpected error occurs") @JsonProperty("timestamp") @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime timestamp,
+        @Schema(description = "Generic response when an unexpected error occurs") @JsonProperty("timestamp") LocalDateTime timestamp,
         @JsonProperty("status") int status, @JsonProperty("error") String error,
         @JsonProperty("message") String message, @JsonProperty("path") String path) {
 }
