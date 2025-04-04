c571fd8e0d3e79c0603a88ff5620cfe18252a7f6
Albert Rodríguez <albert.rodriguez@in2.es>
2025-04-04 12:29:39 +0200

    refactor(backend): imports

--- src/test/java/es/in2/issuer/shared/domain/model/dto/GlobalErrorMessageTest.java
+++ src/test/java/es/in2/issuer/shared/domain/model/dto/GlobalErrorMessageTest.java
@@ -1,6 +1,5 @@
-package es.in2.issuer.backend.domain.model.dto;
+package es.in2.issuer.shared.domain.model.dto;
 
-import es.in2.issuer.shared.domain.model.dto.GlobalErrorMessage;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
