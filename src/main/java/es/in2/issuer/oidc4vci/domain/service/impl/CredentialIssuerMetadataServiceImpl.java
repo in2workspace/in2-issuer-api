64872979ea3cdf9d201fe33f7c5ef53717f3c4fe
Albert Rodríguez <albert.rodriguez@in2.es>
2025-04-04 11:55:51 +0200

    refactor(backend): imports

--- src/main/java/es/in2/issuer/oidc4vci/domain/service/impl/CredentialIssuerMetadataServiceImpl.java
+++ src/main/java/es/in2/issuer/oidc4vci/domain/service/impl/CredentialIssuerMetadataServiceImpl.java
@@ -1,10 +1,9 @@
 package es.in2.issuer.oidc4vci.domain.service.impl;
 
 import es.in2.issuer.oidc4vci.domain.model.dto.CredentialConfiguration;
-import es.in2.issuer.shared.domain.model.dto.CredentialDefinition;
 import es.in2.issuer.oidc4vci.domain.model.dto.CredentialIssuerMetadata;
 import es.in2.issuer.oidc4vci.domain.service.CredentialIssuerMetadataService;
-import es.in2.issuer.shared.domain.util.Constants;
+import es.in2.issuer.shared.domain.model.dto.CredentialDefinition;
 import es.in2.issuer.shared.infrastructure.config.AppConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +13,7 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 import java.util.Map;
 
+import static es.in2.issuer.shared.domain.util.Constants.*;
 import static es.in2.issuer.shared.domain.util.EndpointsConstants.*;
 import static es.in2.issuer.shared.domain.util.HttpUtils.ensureUrlHasProtocol;
 
@@ -28,10 +28,10 @@ public class CredentialIssuerMetadataServiceImpl implements CredentialIssuerMeta
     public Mono<CredentialIssuerMetadata> generateOpenIdCredentialIssuer() {
         String credentialIssuerDomain = ensureUrlHasProtocol(appConfig.getIssuerApiExternalDomain());
         CredentialConfiguration learCredentialEmployee = CredentialConfiguration.builder()
-                .format(Constants.JWT_VC_JSON)
+                .format(JWT_VC_JSON)
                 .cryptographicBindingMethodsSupported(List.of())
                 .credentialSigningAlgValuesSupported(List.of())
-                .credentialDefinition(CredentialDefinition.builder().type(List.of(Constants.LEAR_CREDENTIAL, Constants.VERIFIABLE_CREDENTIAL)).build())
+                .credentialDefinition(CredentialDefinition.builder().type(List.of(LEAR_CREDENTIAL, VERIFIABLE_CREDENTIAL)).build())
                 .build();
         return Mono.just(
                 CredentialIssuerMetadata.builder()
@@ -39,7 +39,7 @@ public class CredentialIssuerMetadataServiceImpl implements CredentialIssuerMeta
                         .credentialEndpoint(credentialIssuerDomain + CREDENTIAL)
                         .batchCredentialEndpoint(credentialIssuerDomain + CREDENTIAL_BATCH)
                         .deferredCredentialEndpoint(credentialIssuerDomain + CREDENTIAL_DEFERRED)
-                        .credentialConfigurationsSupported(Map.of(Constants.LEAR_CREDENTIAL_EMPLOYEE, learCredentialEmployee))
+                        .credentialConfigurationsSupported(Map.of(LEAR_CREDENTIAL_EMPLOYEE, learCredentialEmployee))
                         .build()
         );
     }
