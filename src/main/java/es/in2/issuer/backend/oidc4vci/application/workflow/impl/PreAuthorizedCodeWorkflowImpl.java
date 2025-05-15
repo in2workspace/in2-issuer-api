f9ee3c5d5189c9e142ee78230bbe0c2010826b4d
Albert Rodríguez <albert.rodriguez@in2.es>
2025-05-15 14:57:28 +0200

    fix(workflow): remove unnecessary new line

--- src/main/java/es/in2/issuer/backend/oidc4vci/application/workflow/impl/PreAuthorizedCodeWorkflowImpl.java
+++ src/main/java/es/in2/issuer/backend/oidc4vci/application/workflow/impl/PreAuthorizedCodeWorkflowImpl.java
@@ -17,8 +17,7 @@ public class PreAuthorizedCodeWorkflowImpl implements PreAuthorizedCodeWorkflow 
     private final PreAuthorizedCodeService preAuthorizedCodeService;
 
     @Override
-    public Mono<PreAuthorizedCodeResponse>
-    generatePreAuthorizedCode(Mono<UUID> credentialIdMono) {
+    public Mono<PreAuthorizedCodeResponse> generatePreAuthorizedCode(Mono<UUID> credentialIdMono) {
         String processId = UUID.randomUUID().toString();
         return preAuthorizedCodeService.generatePreAuthorizedCode(processId, credentialIdMono)
                 .doFirst(() -> log.info("ProcessId: {} AuthServer: Starting PreAuthorizedCode generation", processId))
